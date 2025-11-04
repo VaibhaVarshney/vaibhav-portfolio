6069f7216cd23fab6947990788c8f5453758e2be
Vaibhav Varshney <varshney2vaibhav@gmail.com>
2025-11-04 13:59:59 -0500

    chore: add package-lock.json and ignore node_modules

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/vaibhav-portfolio/", // 👈 this line fixes the blank page
-});
